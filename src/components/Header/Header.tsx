016646cdd6eeb878add1ff2f072860c79792944f
Adammm <adammm.web@gmail.com>
2022-12-29 23:19:10 +0300

    component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div>
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/005/130/856/small_2x/letter-v-logo-with-minimal-design-premium-brand-identity-with-business-card-template-vector.jpg" alt="logo"/>
+        </div>
     )
 }
 
