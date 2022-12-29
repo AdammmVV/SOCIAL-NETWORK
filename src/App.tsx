a72be2e42ec9fcfe92df7ccaff7ac788a5987f2c
Adammm <adammm.web@gmail.com>
2022-12-29 23:19:55 +0300

    component changes APP

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
         <Header />
         <NavBar />
+
      <ul>
          <li>css</li>
          <li>html</li>
@@ -20,13 +21,10 @@ function App() {
 const NavBar = () => {
     return (
         <div>
-            <a href="#">Dialog</a>
-            <a href="#">Message</a>
-            <a href="#">Dialog</a>
-            <a href="#">Dialog</a>
-            <a href="#">Dialog</a>
+
         </div>
-    )
+    );
 }
+
 
 export default App;
