e32248c3bfbd568cd1ceefd0a62f2227fb2e84e3
Adammm <adammm.web@gmail.com>
2022-12-29 23:41:26 +0300

    component NavBar

---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div>
+            <div>Profile</div>
+            <div>Message</div>
+            <div>News</div>
+            <div>Music</div>
+            <div>Setting</div>
+        </div>
+    );
+}
+
+
+export default NavBar;
