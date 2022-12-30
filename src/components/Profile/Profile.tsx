6530ad98638330ecaf603fdd0401e6f11a85cf58
Adammm <adammm.web@gmail.com>
2022-12-31 01:00:23 +0300

    changes Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,7 +3,26 @@ import React from "react";
 const Content = () => {
     return (
         <div>
-            <div className="content">My content</div>
+            <div className="content">
+                <div>
+                    <img src="https://www.adlertreks.com/wp-content/uploads/2016/07/IMG_0965-850x350.jpg" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
         </div>
     );
 }
