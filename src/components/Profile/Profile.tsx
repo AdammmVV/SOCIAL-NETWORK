2b7157dafc2425b93661896f44c7b1d9a605d64d
Adammm <adammm.web@gmail.com>
2023-01-13 00:45:53 +0300

    refactor Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
     return (
         <div>
-            <div className={s.content}>
-                <div>
-                    <img src="https://www.adlertreks.com/wp-content/uploads/2016/07/IMG_0965-850x350.jpg" alt=""/>
-                </div>
-                <MyPosts />
-            </div>
+            <ProfileInfo />
+            <MyPosts />
         </div>
     );
 }
