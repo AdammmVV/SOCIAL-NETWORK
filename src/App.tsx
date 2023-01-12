a7fd172ebad8cb8cd1bf78798009e14151eb9e8f
Adammm <adammm.web@gmail.com>
2023-01-12 23:06:43 +0300

    add Route for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
-import NavBar from "./components/Navbar/NavBar";
-import Content from './components/Profile/Profile';
+import {NavBar} from "./components/Navbar/NavBar";
+import {Profile} from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import {Route} from "react-router-dom";
+import {Music} from "./components/Music/Music";
+import {News} from "./components/News/News";
+import {Setting} from "./components/Setting/Setting";
 
-const App = () => {
-  return (
-    <div className="app-wrapper">
-        <Header />
-        <NavBar />
-        <Content />
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <NavBar/>
+            <div className='app-wrapperContent'>
+                <Route path={'/dialogs'} component={Dialogs}/>
+                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/news'} component={News}/>
+                <Route path={'/music'} component={Music}/>
+                <Route path={'/settings'} component={Setting}/>
+            </div>
+        </div>
+    );
 }
 
-export default App;
