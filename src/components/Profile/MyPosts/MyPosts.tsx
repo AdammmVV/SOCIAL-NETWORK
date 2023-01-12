eb1ac68747009ba6938048a260c9b9e79fe2c7a9
Adammm <adammm.web@gmail.com>
2023-01-13 00:46:09 +0300

    refactor MyPosts.tsx

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,16 +5,6 @@ import Post from "./Post/Post";
 const MyPosts = () => {
     return (
         <div className={s.myPosts}>
-            <div>
-                ava + description
-            </div>
-            <div>
-                My posts
-            </div>
-            <div>
-                <textarea></textarea>
-                <button>Add post</button>
-            </div>
             <Post message='How are you?' likeCount='12'/>
             <Post message='Hi, I am Adam.' likeCount='11'/>
             <Post message='Yo!' likeCount='17'/>
