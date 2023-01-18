d8b31b6830903b8bcff7554f42193f2d4120b478
Adammm <adammm.web@gmail.com>
2023-01-19 02:28:28 +0300

    move state up

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,23 @@ import {Route} from "react-router-dom";
 import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
 import {Setting} from "./components/Setting/Setting";
+import {StateType} from "./index";
 
-export const App = () => {
+type AppPropsType = {
+    state: StateType
+}
+
+export const App = (props: AppPropsType) => {
     return (
         <div className="app-wrapper">
             <Header/>
             <NavBar/>
             <div className='app-wrapperContent'>
-                <Route path={'/dialogs'} component={Dialogs}/>
-                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/dialogs'} component={()=><Dialogs
+                    dialogs={props.state.dialogs}
+                    messages={props.state.messages}/>}/>
+                <Route path={'/profile'} component={()=><Profile
+                    posts={props.state.posts}/>}/>
                 <Route path={'/news'} component={News}/>
                 <Route path={'/music'} component={Music}/>
                 <Route path={'/settings'} component={Setting}/>
