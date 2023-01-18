78310f1ab21f162b63f848fd35b6d598d7d71703
Adammm <adammm.web@gmail.com>
2023-01-19 02:29:42 +0300

    moved state up and refactored

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,29 +1,20 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
-import {v1} from "uuid";
+import {PostsType} from "../../../index";
 
-const MyPosts = () => {
+type MyPostsPropsType = {
+    posts: PostsType[]
+}
 
-    type PropsDataType = {
-        id:string
-        message: string
-        likeCount: string
-    }
+const MyPosts = (props: MyPostsPropsType) => {
 
-    let propsData: PropsDataType[] = [
-        {id: v1(), message: 'How are you?', likeCount: '12'},
-        {id: v1(), message: 'Hi, I am Adam.', likeCount: '11'},
-        {id: v1(), message: 'Yo!', likeCount: '17'},
-    ]
-    let mapPost = propsData.map((p) => {
-        return (
-            <Post key={p.id} message={p.message} likeCount={p.likeCount}/>
-        )
-    })
+    let post = props.posts.map(p =>
+        <Post key={p.id} message={p.message} likeCount={p.likeCount}/>)
+
     return (
         <div className={s.myPosts}>
-            {mapPost}
+            {post}
         </div>
     );
 }
