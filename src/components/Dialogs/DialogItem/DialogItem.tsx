261467bdc0c4b9c35e83bb3a805053ac9c69e13e
Adammm <adammm.web@gmail.com>
2023-01-19 00:51:17 +0300

    create DialogItem.tsx

---
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -0,0 +1,15 @@
+import s from "../Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+type DialogItemPropsType = {
+    id: string
+    name: string
+}
+export const DialogItem = (props: DialogItemPropsType) => {
+    return (
+        <div className={s.dialog}>
+            <NavLink to={`'/dialogs/ ${props.id}`} activeClassName={s.active}>{props.name}</NavLink>
+        </div>
+    )
+}
