ea0ebd89ba2733dd70382072676bdfc07eaa8fce
Adammm <adammm.web@gmail.com>
2023-01-19 00:50:58 +0300

    change id module uuid

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {v1} from "uuid";
 
 const MyPosts = () => {
 
     type PropsDataType = {
+        id:string
         message: string
         likeCount: string
     }
 
     let propsData: PropsDataType[] = [
-        {message: 'How are you?', likeCount: '12'},
-        {message: 'Hi, I am Adam.', likeCount: '11'},
-        {message: 'Yo!', likeCount: '17'},
+        {id: v1(), message: 'How are you?', likeCount: '12'},
+        {id: v1(), message: 'Hi, I am Adam.', likeCount: '11'},
+        {id: v1(), message: 'Yo!', likeCount: '17'},
     ]
     let mapPost = propsData.map((p) => {
         return (
-            <Post message={p.message} likeCount={p.likeCount}/>
+            <Post key={p.id} message={p.message} likeCount={p.likeCount}/>
         )
     })
     return (
