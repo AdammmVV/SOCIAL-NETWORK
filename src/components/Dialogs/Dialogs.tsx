a48989544aadca36cb9099346e05f001d66c95f9
Adammm <adammm.web@gmail.com>
2023-01-19 02:29:38 +0300

    moved state up and refactored

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,55 +1,30 @@
 import React from "react";
 import s from './Dialogs.module.css'
-import {v1} from "uuid";
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
+import {DialogsType, MessagesType} from "../../index";
 
-export type PropsDialogsDataType = {
-    id: string
-    name: string
+type DialogsPropsType = {
+    dialogs: DialogsType[]
+    messages: MessagesType[]
 }
 
-export type PropsMessageDataType = {
-    id: string
-    message: string
-}
+const Dialogs = (props: DialogsPropsType) => {
+    let dialogItem = props.dialogs.map(d =>
+        <DialogItem key={d.id} name={d.name} id={d.id}/>)
 
+    let message = props.messages.map(m =>
+        <Message key={m.id} message={m.message}/>)
 
-let propsDialogsData:PropsDialogsDataType[] = [
-    {id: v1(), name: 'Adammm'},
-    {id: v1(), name: 'Dron'},
-    {id: v1(), name: 'Alena'},
-    {id: v1(), name: 'Valentina'},
-]
-
-let propsMessageData:PropsMessageDataType[] = [
-    {id: v1(), message: 'I`m Valery'},
-    {id: v1(), message: 'How are you?'},
-    {id: v1(), message: 'I`m Alena'},
-]
-
-let mapDialogItem = propsDialogsData.map(d => {
-    return (
-        <DialogItem key={d.id} name={d.name} id={d.id}/>
-    )
-})
-
-let mapMessage = propsMessageData.map(m => {
-    return (
-        <Message key={m.id} message={m.message} />
-    )
-})
-
-const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 <div className={s.dialog}>
-                    {mapDialogItem}
+                    {dialogItem}
                 </div>
             </div>
             <div className={s.messages}>
-                {mapMessage}
+                {message}
             </div>
         </div>
     )
