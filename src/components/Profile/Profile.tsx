18f5e4b276a7e7d853635d97a76dcf40e97ebf9c
Adammm <adammm.web@gmail.com>
2023-01-19 02:29:45 +0300

    moved state up and refactored

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
+import {PostsType} from "../../index";
 
-export const Profile = () => {
+type ProfilePropsType = {
+    posts: PostsType[]
+}
+
+export const Profile = (props: ProfilePropsType) => {
     return (
         <div>
             <ProfileInfo />
-            <MyPosts />
+            <MyPosts posts={props.posts}/>
         </div>
     );
 }
