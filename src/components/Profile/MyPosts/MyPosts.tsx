7fe231a3fd6d17b8640b877e9e1333bf1917fb66
Adammm <adammm.web@gmail.com>
2023-01-19 18:06:43 +0300

    change MyPosts.tsx

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
-import {PostsType} from "../../../index";
+import {profilePageType} from "../../../redux/state";
 
 type MyPostsPropsType = {
-    posts: PostsType[]
+    profilePage: profilePageType
 }
 
 const MyPosts = (props: MyPostsPropsType) => {
 
-    let post = props.posts.map(p =>
+    let post = props.profilePage.posts.map(p =>
         <Post key={p.id} message={p.message} likeCount={p.likeCount}/>)
 
     return (
