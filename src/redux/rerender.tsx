125f0d45ad9c5ec61b450f2a3cbb63f5e559464d
Adammm <adammm.web@gmail.com>
2023-01-29 00:14:31 +0300

    create rerender.tsx

---
+++ src/redux/rerender.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+import {App} from '../App';
+import {BrowserRouter} from "react-router-dom";
+import {addMessages, addPost, StateType} from "./state";
+
+export const rerenderEnteredTree = (state: StateType) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} addMessages={addMessages}/>
+        </BrowserRouter>
+        , document.getElementById('root')
+    );
+}
