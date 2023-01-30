1457721ebc679ad80d383d3500fef023ecf2e5b5
Adammm <adammm.web@gmail.com>
2023-01-30 20:55:36 +0300

    germs Flax architecture change render.tsx

--- src/redux/rerender.tsx
+++ src/redux/rerender.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import '../index.css';
 import {App} from '../App';
 import {BrowserRouter} from "react-router-dom";
-import {addMessages, addPost, StateType} from "./state";
+import {addMessages, addPost, StateType, updateProfileMessage} from "./state";
 
 export const rerenderEnteredTree = (state: StateType) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} addMessages={addMessages}/>
+            <App state={state}
+                 addPost={addPost}
+                 addMessages={addMessages}
+                 updateProfileMessage={updateProfileMessage}/>
         </BrowserRouter>
         , document.getElementById('root')
     );
