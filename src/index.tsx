af1d1e677b6c70cc09ca0f5bc9fc54298fdfc52f
Adammm <adammm.web@gmail.com>
2023-01-31 02:01:38 +0300

    remove cyclic dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
-import React from "react";
-import {rerenderEnteredTree} from "./redux/rerender";
-import {state} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App} from './App';
+import {BrowserRouter} from "react-router-dom";
+import {
+    addMessages,
+    addPost,
+    state,
+    StateType,
+    subscriber,
+    updateDialogMessage,
+    updateProfileMessage
+} from "./redux/state";
 
-
+export const rerenderEnteredTree = (state: StateType) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+                 addPost={addPost}
+                 addMessages={addMessages}
+                 updateProfileMessage={updateProfileMessage}
+                 updateDialogMessage={updateDialogMessage}/>
+        </BrowserRouter>
+        , document.getElementById('root')
+    );
+}
 rerenderEnteredTree(state)
+
+subscriber(rerenderEnteredTree)
