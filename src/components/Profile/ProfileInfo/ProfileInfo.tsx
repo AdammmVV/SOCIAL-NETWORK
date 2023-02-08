1c2ca7acf80a25c82b4f228ea948c6cbf44566cf
Adammm <adammm.web@gmail.com>
2023-02-08 17:21:52 +0300

    refactor ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -7,6 +7,8 @@ type ProfileInfoPropsType = {
     updateProfileMessage: (val:string) => void
 }
 
+const logo = 'https://обложка-вк.рф/src/img/lower/it/it-khakery/it-khakery-1.png'
+
 export const ProfileInfo = (props: ProfileInfoPropsType) => {
 
     const onClickButtonHandler = () => {
@@ -19,20 +21,24 @@ export const ProfileInfo = (props: ProfileInfoPropsType) => {
 
     return (
         <div className={s.content}>
-            <div>
-                <img src="https://www.adlertreks.com/wp-content/uploads/2016/07/IMG_0965-850x350.jpg" alt=""/>
+            <div className={s.avatarDescriptionContainer}>
+                <img src={logo} alt=""/>
+                <div className={s.avatarDescription}>
+                    <span className={s.ava}>
+                        <img src="https://avatars.githubusercontent.com/u/114234336?s=400&u=733596071371f0b251db3bfeeaef4d0d953bca77&v=4" />
+                        </span>
+                    <span className={s.description}>Валерий Адамчук</span>
+                </div>
             </div>
-            <div>
-                ava + description
-            </div>
-            <div>
+
+            <div className={s.myPosts}>
                 My posts
             </div>
-            <div>
-                <div>
-                    <textarea onChange={onChangeHandler} value={props.profileMessage}/>
+            <div className={s.addPosts}>
+                <div className={s.addPostTextarea}>
+                    <textarea placeholder={'New post'} onChange={onChangeHandler} value={props.profileMessage}/>
                 </div>
-                <div>
+                <div className={s.addPostsButton}>
                     <button onClick={onClickButtonHandler}>Add post</button>
                 </div>
             </div>
