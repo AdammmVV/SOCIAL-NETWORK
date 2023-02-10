6d0bf327744206f364dce638f1e4e7816e10fda5
Adammm <adammm.web@gmail.com>
2023-02-10 15:22:23 +0300

    refactor ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -7,8 +7,6 @@ type ProfileInfoPropsType = {
     dispatch: (action: ActionType) => void
 }
 
-const logo = 'https://обложка-вк.рф/src/img/lower/it/it-khakery/it-khakery-1.png'
-
 export const ProfileInfo = (props: ProfileInfoPropsType) => {
 
     const onClickButtonHandler = () => {
@@ -22,11 +20,10 @@ export const ProfileInfo = (props: ProfileInfoPropsType) => {
     return (
         <div className={s.content}>
             <div className={s.avatarDescriptionContainer}>
-                <img src={logo} alt=""/>
+                <img src='https://обложка-вк.рф/src/img/lower/it/it-khakery/it-khakery-1.png' alt="cover"/>
                 <div className={s.avatarDescription}>
                     <span className={s.ava}>
-                        <img
-                            src="https://avatars.githubusercontent.com/u/114234336?s=400&u=733596071371f0b251db3bfeeaef4d0d953bca77&v=4"/>
+                        <img src="https://avatars.githubusercontent.com/u/114234336?s=400&u=733596071371f0b251db3bfeeaef4d0d953bca77&v=4" alt='avatar'/>
                         </span>
                     <span className={s.description}>Валерий Адамчук</span>
                 </div>
