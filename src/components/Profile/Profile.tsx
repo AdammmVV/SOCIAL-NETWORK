00435766b8cbde7bad51ed9200dfd48232c50c50
Adammm <adammm.web@gmail.com>
2023-02-10 14:01:18 +0300

    refactor Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
-import {profilePageType} from "../../redux/state";
+import {profilePageType} from "../../redux/store";
 
 type ProfilePropsType = {
     profilePage: profilePageType
