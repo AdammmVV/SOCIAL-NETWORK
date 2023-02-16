04c5535b612ce6b478d46b765c71fbd9ae9a78e7
Adammm <adammm.web@gmail.com>
2023-02-17 00:41:17 +0300

    refactor component Profile.tsx add REDUX

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import MyPosts from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
-import {ActionType, ProfilePageType} from "../../redux/store";
+import {StoreType} from "../../redux/store";
+import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
 
 type ProfilePropsType = {
-    profilePage: ProfilePageType
-    profileMessage: string
-    dispatch: (action: ActionType) => void
+    store: StoreType
 }
 
 export const Profile = (props: ProfilePropsType) => {
     return (
         <div>
-            <ProfileInfo profileMessage={props.profileMessage} dispatch={props.dispatch}/>
-            <MyPosts profilePage={props.profilePage}/>
+            <ProfileInfo/>
+            <MyPostsContainer store={props.store}/>
         </div>
     );
 }
