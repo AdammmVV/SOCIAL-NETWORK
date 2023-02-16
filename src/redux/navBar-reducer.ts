4d30233aa6fe2de59d712d4dc76fde9873e08028
Adammm <adammm.web@gmail.com>
2023-02-17 00:44:03 +0300

    refactor reducer navBar-reducer.ts

--- src/redux/navBar-reducer.ts
+++ src/redux/navBar-reducer.ts
@@ -1,5 +1,50 @@
 import {ActionType, NavBarType} from "./store";
+import {v1} from "uuid";
+import logoMan from "../img/logo-man.jpg";
+import logoWoman from "../img/logoWoman.jpg";
 
-export const navBarReducer = (action: ActionType, state: NavBarType) => {
+let initialState: NavBarType = {
+    nav: [
+        {
+            id: v1(),
+            name: 'Andrey',
+            avatar: `${logoMan}`,
+            online: true,
+        },
+        {
+            id: v1(),
+            name: 'Vladimir',
+            avatar: `${logoMan}`,
+            online: true,
+        },
+        {
+            id: v1(),
+            name: 'Alena',
+            avatar: `${logoWoman}`,
+            online: false,
+        },
+        {
+            id: v1(),
+            name: 'Natasha',
+            avatar: `${logoWoman}`,
+            online: false,
+        },
+        {
+            id: v1(),
+            name: 'Roma',
+            avatar: `${logoMan}`,
+            online: true,
+        },
+        {
+            id: v1(),
+            name: 'Valentina',
+            avatar: `${logoWoman}`,
+            online: false,
+        },
+    ],
+}
+
+
+export const navBarReducer = (state:NavBarType = initialState, action: ActionType) => {
     return state
 }
