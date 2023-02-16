5526b2e955510ffe0709f5bf10ccd26e4b50ee2b
Adammm <adammm.web@gmail.com>
2023-02-17 00:44:36 +0300

    refactor index.tsx add redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
-import {StateType, store} from "./redux/store";
+import {StateType} from "./redux/store";
+import {store} from "./redux/redux-store";
 
 export const rerenderEnteredTree = (state: StateType) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)}/>
+            <App store={store}/>
         </BrowserRouter>
         , document.getElementById('root')
     );
 }
 rerenderEnteredTree(store.getState())
 
-store.subscriber(rerenderEnteredTree)
+store.subscribe(()=> {
+    let state = store.getState()
+    rerenderEnteredTree(state)
+})
