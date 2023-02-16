912771236d923613f345da89d166362ba385b1b5
Adammm <adammm.web@gmail.com>
2023-02-17 00:43:48 +0300

    refactor reducer profile-reducer.ts

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,7 +1,17 @@
 import {v1} from "uuid";
 import {ActionType, ProfilePageType} from "./store";
 
-export const profileReducer = (action: ActionType, state: ProfilePageType) => {
+let initialState: ProfilePageType = {
+    posts: [
+        {id: v1(), message: 'How are you?', likeCount: '12'},
+        {id: v1(), message: 'Hi, I am Adam.', likeCount: '11'},
+        {id: v1(), message: 'Yo!', likeCount: '17'},
+    ],
+    profileMessage: '',
+}
+
+
+export const profileReducer = (state: ProfilePageType = initialState, action: ActionType) => {
     if (action.type === "ADD-POST") {
         let post = {id: v1(), message: state.profileMessage, likeCount: '0'}
         state.posts.push(post)
