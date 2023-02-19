8ad1c8ba66373e2988b3ce69bf63612000ad8168
Adammm <adammm.web@gmail.com>
2023-02-19 22:43:04 +0300

    fix index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
 import {StateType} from "./redux/store";
 import {store} from "./redux/redux-store";
+import {StoreContext} from "./StoreContext";
 
 export const rerenderEnteredTree = (state: StateType) => {
     ReactDOM.render(
-        <BrowserRouter>
-            <App store={store}/>
-        </BrowserRouter>
+        <StoreContext.Provider value={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </StoreContext.Provider>
         , document.getElementById('root')
     );
 }
