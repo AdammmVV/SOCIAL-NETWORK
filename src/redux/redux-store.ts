c59094d382a457e26c202ac149c0b398870ecceb
Adammm <adammm.web@gmail.com>
2023-02-26 23:44:08 +0300

    create userPage and refactor store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,14 +2,17 @@ import {combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogReducer} from "./dialog-reducer";
 import {navBarReducer} from "./navBar-reducer";
+import {usersReducer} from "./users-reducer";
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogReducer,
     navBar: navBarReducer,
+    usersPage: usersReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
 
 export let store = createStore(rootReducer);
+console.log(store.getState())
