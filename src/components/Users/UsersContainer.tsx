dd8def84a2b0c2e8c172a41f1c4ee0c97bcdc9fb
Adammm <adammm.web@gmail.com>
2023-02-26 23:45:11 +0300

    create UsersContainer.tsx

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,17 +1,38 @@
 import React from "react";
 import {connect} from "react-redux";
 import {Users} from "./Users";
+import {followAC, InitialStateUsersType, setUsersAC, unfollowAC, UsersType} from "../../redux/users-reducer";
+import {Dispatch} from "redux";
+import {AppStateType} from "../../redux/redux-store";
 
-const mapStateToProps = () => {
-  return {
-
-  }
+type MapStateToPropsType = {
+    usersPage: InitialStateUsersType
 }
 
-const mapDispatchToProps = () => {
-  return {
+type MapDispatchToPropsType = {
+    follow: (userId: string) => void
+    unfollow: (userId: string) => void
+    setUsers: (newUsers: UsersType[]) => void
+}
 
-  }
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
+    return {
+        usersPage: state.usersPage
+    }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
+    return {
+        follow: (userId: string) => {
+            dispatch(followAC(userId))
+        },
+        unfollow: (userId: string) => {
+            dispatch(unfollowAC(userId))
+        },
+        setUsers: (newUsers: UsersType[]) => {
+            dispatch(setUsersAC(newUsers))
+        },
+    }
 }
 
 export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
