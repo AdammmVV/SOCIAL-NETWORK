4f720ca0def51a9129ddf790df437a026e42f72a
Adammm <adammm.web@gmail.com>
2023-02-26 23:43:32 +0300

    refactor index.tsx removed function rerenderEnteredTree and subscribe

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
-import {AppStateType, store} from "./redux/redux-store";
-import { Provider } from 'react-redux';
+import {store} from "./redux/redux-store";
+import {Provider} from 'react-redux';
 
-export const rerenderEnteredTree = (state: AppStateType) => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
-        , document.getElementById('root')
-    );
-}
-rerenderEnteredTree(store.getState())
-
-store.subscribe(()=> {
-    let state = store.getState()
-    rerenderEnteredTree(state)
-})
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
