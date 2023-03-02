95a9907c84f62174ebd079db0cfe9eec57fa8ed1
Adammm <adammm.web@gmail.com>
2023-03-02 23:20:42 +0300

    refactor user-reducer.ts for server-data

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -2,17 +2,29 @@ export type InitialStateUsersType = {
     users: UsersType[]
 }
 
+// export type UsersType = {
+//     id: string
+//     name: string
+//     description: string
+//     avatar: string
+//     online: boolean
+//     follow: boolean
+//     address: {
+//         cityName: string
+//         country: string
+//     }
+// }
+
 export type UsersType = {
-    id: string
     name: string
-    description: string
-    avatar: string
-    online: boolean
-    follow: boolean
-    address: {
-        cityName: string
-        country: string
+    id: number
+    uniqueUrlName: null | string
+    photos: {
+        "small": null | string
+        "large": null | string
     }
+    status: string
+    followed: boolean
 }
 
 export const initialState: InitialStateUsersType = {
@@ -22,9 +34,9 @@ export const initialState: InitialStateUsersType = {
 export const usersReducer = (state: InitialStateUsersType = initialState, action: MainAT) => {
     switch (action.type) {
         case 'FOLLOW':
-            return {...state, users: state.users.map(u => u.id === action.payload.userId ? {...u, follow: true} : u)}
+            return {...state, users: state.users.map(u => u.id === action.payload.userId ? {...u, followed: true} : u)}
         case 'UNFOLLOW':
-            return {...state, users: state.users.map(u => u.id === action.payload.userId ? {...u, follow: false} : u)}
+            return {...state, users: state.users.map(u => u.id === action.payload.userId ? {...u, followed: false} : u)}
         case 'SET-USERS':
             console.log(action.payload.newUsers)
             return {...state, users: [...action.payload.newUsers]}
@@ -39,7 +51,7 @@ type FollowAT = ReturnType<typeof followAC>
 type UnfollowAT = ReturnType<typeof unfollowAC>
 type SetUsersAT = ReturnType<typeof setUsersAC>
 
-export const followAC = (userId: string) => {
+export const followAC = (userId: number) => {
     return {
         type: 'FOLLOW',
         payload: {
@@ -48,7 +60,7 @@ export const followAC = (userId: string) => {
     } as const
 }
 
-export const unfollowAC = (userId: string) => {
+export const unfollowAC = (userId: number) => {
     return {
         type: 'UNFOLLOW',
         payload: {
