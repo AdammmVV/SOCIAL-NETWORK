5a810b086ebcb0c63dd4c3a9561a735828f2d769
Adammm <adammm.web@gmail.com>
2023-03-07 01:28:01 +0300

    refactor UsersContainer.tsx and added class container for Users.tsx

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import {connect} from "react-redux";
-import {followAC, InitialStateUsersType, setUsersAC, unfollowAC, UsersType} from "../../redux/users-reducer";
+import {
+    followAC,
+    InitialStateUsersType, setIsFetchingAC,
+    setNumberPageAC,
+    setUsersAC,
+    unfollowAC,
+} from "../../redux/users-reducer";
 import {Dispatch} from "redux";
 import {AppStateType} from "../../redux/redux-store";
 import {Users} from "./Users";
+import axios from "axios";
 
 type MapStateToPropsType = {
     usersPage: InitialStateUsersType
@@ -12,7 +19,49 @@ type MapStateToPropsType = {
 type MapDispatchToPropsType = {
     follow: (userId: number) => void
     unfollow: (userId: number) => void
-    setUsers: (newUsers: UsersType[]) => void
+    setUsers: (newUsers: InitialStateUsersType) => void
+    setNumberPage: (numberPage: number) => void
+    setIsFetching: (isFetching: boolean) => void
+}
+
+type UsersPropsType = {
+    usersPage: InitialStateUsersType,
+    follow: (userId: number) => void,
+    unfollow: (userId: number) => void,
+    setUsers: (newUsers: InitialStateUsersType) => void,
+    setNumberPage: (numberPage: number) => void
+    setIsFetching: (isFetching: boolean) => void
+
+}
+
+export class UsersAPIComponent extends React.Component<UsersPropsType> {
+
+    componentDidMount() {
+        this.props.setIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersPage.countPage}&page=${this.props.usersPage.numberPage}`)
+            .then(response => {
+                this.props.setIsFetching(false)
+                this.props.setUsers(response.data)
+            })
+    }
+
+    setNumberPageHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        this.props.setNumberPage(+e.currentTarget.value)
+        this.props.setIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersPage.countPage}&page=${+e.currentTarget.value}`)
+            .then(response => {
+                this.props.setIsFetching(false)
+                this.props.setUsers(response.data)
+            })
+    }
+
+    render() {
+        return <Users usersPage={this.props.usersPage}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+                      setNumberPageHandler={this.setNumberPageHandler}
+        />
+    }
 }
 
 const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
@@ -29,10 +78,16 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
         unfollow: (userId: number) => {
             dispatch(unfollowAC(userId))
         },
-        setUsers: (newUsers: UsersType[]) => {
+        setUsers: (newUsers: InitialStateUsersType) => {
             dispatch(setUsersAC(newUsers))
         },
+        setNumberPage: (numberPage: number) => {
+            dispatch(setNumberPageAC(numberPage))
+        },
+        setIsFetching: (isFetching: boolean) => {
+            dispatch(setIsFetchingAC(isFetching))
+        }
     }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
