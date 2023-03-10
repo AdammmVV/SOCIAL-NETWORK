478f847b402681333c5fc5fd021d15a016108166
Adammm <adammm.web@gmail.com>
2023-03-10 13:38:24 +0300

    server request for Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
+import {ProfileInfoType} from "../../redux/profile-reducer";
 
-export const Profile = () => {
+type ProfilePropsType = {
+    profileInfo: ProfileInfoType
+    isFetching: boolean
+}
+
+export const Profile = (props: ProfilePropsType) => {
     return (
         <div>
-            <ProfileInfo/>
+            <ProfileInfo {...props}/>
             <MyPostsContainer/>
         </div>
     );
