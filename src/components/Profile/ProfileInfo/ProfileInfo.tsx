9921b8723ed74e99167f4829809858e8fbdf1dd6
Adammm <adammm.web@gmail.com>
2023-03-10 13:40:26 +0300

    refactor component ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,22 +1,56 @@
 import s from "./ProfileInfo.module.css";
 import React from "react";
+import {ProfileInfoType} from "../../../redux/profile-reducer";
+import ava from '../../../img/avaEvery.jpg'
+import {Preloader} from "../../common/preloader/Preloader";
 
+type ProfileInfoPropsType = {
+    profileInfo: ProfileInfoType
+    isFetching: boolean
+}
 
-export const ProfileInfo = () => {
+export const ProfileInfo = (props: ProfileInfoPropsType) => {
 
     return (
         <div className={s.content}>
+            { props.isFetching && <Preloader/> }
             <div className={s.avatarDescriptionContainer}>
                 <img src='https://обложка-вк.рф/src/img/lower/it/it-khakery/it-khakery-1.png' alt="cover"/>
                 <div className={s.avatarDescription}>
                     <span className={s.ava}>
                         <img
-                            src="https://avatars.githubusercontent.com/u/114234336?s=400&u=733596071371f0b251db3bfeeaef4d0d953bca77&v=4"
+                            src={props.profileInfo.photos.small ? props.profileInfo.photos.small : ava}
                             alt='avatar'/>
                         </span>
-                    <span className={s.description}>Валерий Адамчук</span>
+                    <div className={s.description}>
+                        <span>{props.profileInfo.fullName}</span>
+                        <span>О мне: {props.profileInfo.aboutMe}</span>
+                        <div>
+                            Контакты:
+                            <span>{props.profileInfo.contacts.facebook}</span>
+                            <span>{props.profileInfo.contacts.vk}</span>
+                            <span>{props.profileInfo.contacts.github}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
+// return (
+//     <div className={s.content}>
+//         <div className={s.avatarDescriptionContainer}>
+//             <img src='https://обложка-вк.рф/src/img/lower/it/it-khakery/it-khakery-1.png' alt="cover"/>
+//             <div className={s.avatarDescription}>
+//                     <span className={s.ava}>
+//                         <img
+//                             src="https://avatars.githubusercontent.com/u/114234336?s=400&u=733596071371f0b251db3bfeeaef4d0d953bca77&v=4"
+//                             alt='avatar'/>
+//                         </span>
+//                 <span className={s.description}>Валерий Адамчук</span>
+//             </div>
+//         </div>
+//     </div>
+// )
