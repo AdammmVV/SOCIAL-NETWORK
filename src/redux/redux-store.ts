1a90f4ca86e0c2eee37a0ed772f0636ebd71626b
Adammm <adammm.web@gmail.com>
2023-03-16 17:28:21 +0300

    62 lesson redux-store.ts

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,12 +3,14 @@ import {profileReducer} from "./profile-reducer";
 import {dialogReducer} from "./dialog-reducer";
 import {navBarReducer} from "./navBar-reducer";
 import {usersReducer} from "./users-reducer";
+import {authReducer} from "./auth-reducer";
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogReducer,
     navBar: navBarReducer,
     usersPage: usersReducer,
+    auth: authReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
