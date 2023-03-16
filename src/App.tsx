94d2ebb2b39aeaa55a3b1d2aa3faf83a1273b6de
Adammm <adammm.web@gmail.com>
2023-03-16 17:26:29 +0300

    62 lesson refactor component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import {Route} from "react-router-dom";
 import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
@@ -9,12 +8,13 @@ import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 import {NavBarContainer} from "./components/Navbar/NavBarContainer";
 import {UsersContainer} from "./components/Users/UsersContainer";
 import {ProfileContainer} from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 export const App = () => {
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <NavBarContainer/>
             <div className='app-wrapperContent'>
                 <Route path={'/dialogs'} component={DialogsContainer}/>
