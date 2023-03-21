d3d85633eeb659d9461b45280e9698cde44c271e
Adammm <adammm.web@gmail.com>
2023-03-22 00:14:16 +0300

    66 lesson create thunk for Profile

--- src/redux/TestsReducers/profile-reducer.test.ts
+++ src/redux/TestsReducers/profile-reducer.test.ts
@@ -1,5 +1,10 @@
 import {v1} from "uuid";
-import {addPostActionCreator, profileReducer, updateProfileMessageActionCreator} from "../profile-reducer";
+import {
+    addPostActionCreator,
+    ProfileInfoType,
+    profileReducer,
+    updateProfileMessageActionCreator
+} from "../profile-reducer";
 
 export type PostsType = {
     id: string
@@ -7,18 +12,40 @@ export type PostsType = {
     likeCount: string
 }
 
-test('post should be correct added', ()=> {
+test('post should be correct added', () => {
 
     let initialState = {
-    posts: [
-        {id: v1(), message: 'How are you?', likeCount: '12'},
-        {id: v1(), message: 'Hi, I am Adam.', likeCount: '11'},
-        {id: v1(), message: 'Yo!', likeCount: '17'},
-    ] as PostsType[],
-    profileMessage: '',
-}
+        profileInfo: {
+            aboutMe: null,
+            contacts: {
+                facebook: null,
+                website: null,
+                vk: null,
+                twitter: null,
+                instagram: null,
+                youtube: null,
+                github: null,
+                mainLink: null
+            },
+            lookingForAJob: false,
+            lookingForAJobDescription: null,
+            fullName: "NazarioSaf",
+            userId: 28317,
+            photos: {
+                small: null,
+                large: null
+            }
+        } as ProfileInfoType,
+        posts: [
+            {id: v1(), message: 'How are you?', likeCount: '12'},
+            {id: v1(), message: 'Hi, I am Adam.', likeCount: '11'},
+            {id: v1(), message: 'Yo!', likeCount: '17'},
+        ] as PostsType[],
+        isFetching: false,
+        profileMessage: '',
+    }
 
- const endState = profileReducer(initialState ,addPostActionCreator())
+    const endState = profileReducer(initialState, addPostActionCreator())
 
     expect(endState).not.toBe(initialState)
     expect(endState.profileMessage).toBe('')
@@ -26,13 +53,35 @@ test('post should be correct added', ()=> {
 
 })
 
-test('profileMessage should be correct updated', ()=> {
+test('profileMessage should be correct updated', () => {
     let initialState = {
+        profileInfo: {
+            aboutMe: null,
+            contacts: {
+                facebook: null,
+                website: null,
+                vk: null,
+                twitter: null,
+                instagram: null,
+                youtube: null,
+                github: null,
+                mainLink: null
+            },
+            lookingForAJob: false,
+            lookingForAJobDescription: null,
+            fullName: "NazarioSaf",
+            userId: 28317,
+            photos: {
+                small: null,
+                large: null
+            }
+        } as ProfileInfoType,
         posts: [
             {id: v1(), message: 'How are you?', likeCount: '12'},
             {id: v1(), message: 'Hi, I am Adam.', likeCount: '11'},
             {id: v1(), message: 'Yo!', likeCount: '17'},
         ] as PostsType[],
+        isFetching: false,
         profileMessage: '',
     }
 
