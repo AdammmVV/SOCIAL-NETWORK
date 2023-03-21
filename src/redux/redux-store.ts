b71f9b9553ab4d92bdcf9f2348d6ab83944b219e
Adammm <adammm.web@gmail.com>
2023-03-22 00:15:04 +0300

    66 lesson create thunk middleWeer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogReducer} from "./dialog-reducer";
 import {navBarReducer} from "./navBar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
+import thunk from "redux-thunk";
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -16,4 +17,4 @@ let rootReducer = combineReducers({
 export type AppStateType = ReturnType<typeof rootReducer>
 
 
-export let store = createStore(rootReducer);
+export let store = createStore(rootReducer, applyMiddleware(thunk));
