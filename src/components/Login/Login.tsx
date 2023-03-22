5e99f3c7d04a2fc77060241df240c9f96c084d81
Adammm <adammm.web@gmail.com>
2023-03-23 00:05:48 +0300

    68 lesson create component Login.tsx

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type LoginPropsType = {}
+
+export const Login: React.FC<LoginPropsType> = () => {
+    return (
+        <h2>
+            Login
+        </h2>
+    )
+}
