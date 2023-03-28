d1b1ac5680462d46f338618e361149beea5cdc91
Adammm <adammm.web@gmail.com>
2023-03-28 22:07:29 +0300

    74 lesson create ProfileStatus.tsx

---
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,38 @@
+import React, {ChangeEvent} from "react";
+
+export class ProfileStatus extends React.Component<any, any> {
+    state = {
+        editMode: false,
+        title: this.props.profileStatus
+    }
+
+    activeInput = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactiveInput = () => {
+        this.props.updateStatus(this.state.title)
+        this.setState({
+            editMode: false
+        })
+    }
+
+    onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            title: e.currentTarget.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.editMode
+                    ? <input autoFocus onBlur={this.deactiveInput} onChange={this.onChangeHandler} value={this.state.title}/>
+                    : <span onDoubleClick={this.activeInput}>{this.props.profileStatus ? this.props.profileStatus : 'введите'}</span>
+                }
+            </div>
+        )
+    }
+}
