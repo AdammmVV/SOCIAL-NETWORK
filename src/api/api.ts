a45be9a597f5c34f18cd9771d1c81bc6daef2bd2
Adammm <adammm.web@gmail.com>
2023-06-28 01:54:21 +0300

    update: api and added types

--- src/api/api.ts
+++ src/api/api.ts
@@ -22,35 +22,163 @@ export const usersAPI = {
         const response = await instance.delete(`follow/${userId}`)
         return response.data
     },
-    async getUser(userId: string) {
-        const response = await instance.get(`profile/${userId}`)
-        return response.data
-    },
 }
 
 export const authAPI = {
-    async getMe() {
-        const response = await instance.get(`auth/me`)
-        return response.data
+    getMe: async () => {
+        const res = await instance.get<CommonResponseType<AuthResponse>>(`auth/me`)
+        return res.data
     },
-    logIn(dataLogin: FormDataType) {
-        return instance.post('auth/login', {
+    logIn: async (dataLogin: FormDataType) => {
+        const data = {
             email: dataLogin.email,
             password: dataLogin.password,
             rememberMe: dataLogin.rememberMe
-        }).then(data => data.data)
+        }
+        const res = await instance.post<CommonResponseType<{userId: number}>>('auth/login', data)
+        return res.data
     },
-    logOut() {
-        return instance.delete('auth/login')
-            .then((res) => res.data)
+    logOut: async () => {
+        const res = await instance.delete<CommonResponseType>('auth/login')
+        return res.data
     }
 }
 
 export const profileAPI = {
-    getProfileStatus(userId: string) {
-        return instance.get(`profile/status/${userId}`).then(data => data.data)
+    getProfileStatus: async (userId: string) => {
+        const res = await instance.get<string>(`profile/status/${userId}`)
+        return res.data
     },
-    updateStatus(status: string) {
-        return instance.put('profile/status', {status: status}).then(data => data)
+    updateStatus: async (status: string) => {
+        const res = await instance.put<CommonResponseType>('profile/status', {status: status})
+        return res.data
+    },
+   getProfileUser: async (userId: string) => {
+        const res = await instance.get<GetProfileUserResponseType>(`profile/${userId}`)
+        return res.data
+    },
+    putProfile: async (data: ProfileDomainType) => {
+        const res = await instance.put<CommonResponseType>('profile', data)
+        return res.data
+    },
+    putPhoto: async (data: FormData) => {
+        const res = await instance.put('profile/photo', data)
+        return res.data
+    }
+}
+
+export const dialogAPI = {
+    createDialogWithUser: async (userId: string) => {
+        const res = await instance.put<CommonResponseType>(`dialogs/${userId}`, {})
+        return res.data
+    },
+    getDialogMessagesWithUser: async (userId: string) => {
+        const res = await instance.get<ResponseDialogMessageType>(`dialogs/${userId}/messages`)
+        return res.data
+    },
+    getDialogs: async () => {
+        const res = await instance.get<ResponseDialogsType[]>('dialogs')
+        return res.data
+    },
+    createMessage: async (userId: string, body: {body: string}) => {
+        const res = await instance.post<CommonResponseType<ResponseCreateMessageType>>(`dialogs/${userId}/messages`, body)
+        return res.data
+    },
+    isViewedMessage: async (messageId: string) => {
+        const res = await instance.get<boolean>(`dialogs/messages/${messageId}/viewed`)
+        return res.data
+    },
+    deleteMessage: async (messageId: string) => {
+        const res = await instance.delete<CommonResponseType>(`dialogs/messages/${messageId}`)
+        return res.data
+    },
+    getDialogsCounter: async () => {
+        const res = await instance.get<number>('dialogs/messages/new/count')
+        return res.data
+    }
+}
+
+export type CommonResponseType<D = {}> = {
+    data: D
+    messages: []
+    fieldsErrors: []
+    resultCode: number
+}
+
+type PhotosType = {
+    small: null | string
+    large: null | string
+}
+
+type AuthResponse = {
+    id: number
+    login: string
+    email: string
+}
+
+export type ProfileDomainType = {
+    contacts: {
+        facebook: null | string
+        github: null | string
+        instagram: null | string
+        mainLink: null | string
+        twitter: null | string
+        vk: null | string
+        website: null | string
+        youtube: null | string
+    },
+    lookingForAJob: boolean
+    lookingForAJobDescription: null | string
+    fullName: null | string
+}
+
+export type GetProfileUserResponseType = ProfileDomainType & {
+    userId: number
+    aboutMe: string
+    photos: PhotosType
+}
+
+export type ResponseDialogMessageType = {
+    items: [
+        {
+            id: string
+            body: string
+            translatedBody: null | string
+            addedAt: string
+            senderId: number
+            senderName: string
+            recipientId: number
+            viewed: boolean
+        }
+    ],
+    totalCount: number
+    error: null | string
+}
+
+export type ResponseDialogsType = {
+    id: number
+    userName: string
+    hasNewMessages: boolean
+    lastDialogActivityDate: string
+    lastUserActivityDate: string
+    newMessagesCount: number
+    photos: PhotosType
+}
+
+export type ResponseCreateMessageType = {
+    message: {
+        id: string
+        body: string
+        translatedBody: null
+        addedAt: string
+        senderId: number
+        senderName: string
+        recipientId: number
+        recipientName: string
+        viewed: boolean
+        deletedBySender: boolean
+        deletedByRecipient: boolean
+        isSpam: boolean
+        distributionId: null
     }
 }
